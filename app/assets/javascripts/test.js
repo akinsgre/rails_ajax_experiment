a11dc9f719835cbb9b6f806c69710cde8bf0b220
Greg Akins <angrygreg@gmail.com>
2011-12-08 17:47:58 -0500

    Adding a non-rails form to accept contact adds

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,28 +1,30 @@
 $().ready(function() {
 
-		$('#newContactPopup').dialog({ autoOpen : false, width: '400px', title: 'Add Contact Info' }) ;
-		$('#contactOpenBtn').click( 
-				   function() { 
-				       $('#flash_notice').fadeOut('slow');
-			       $('#newContactPopup').dialog('open');
-			       
-			   }
-			    );
-		$('#newGroupPopup').dialog({ autoOpen : false, width: '400px', title: 'Add Group Info' }) ;
-		$('#groupOpenBtn').click( 
-				   function() { 
-				       $('#flash_notice').fadeOut('slow');
-			       $('#newGroupPopup').dialog('open');
-			       
-			   }
-			    );
-		$('#addContactPopup').dialog({ autoOpen : false, width: '400px', title: 'Subscribe to this Group' }) ;
-		$('#addContactBtn').click( 
-				   function() { 
-				       $('#flash_notice').fadeOut('slow');
-			       $('#addContactPopup').dialog('open');
-			       
-			   }
-			    );
+    $('#newContactPopup').dialog({ autoOpen : false, width: '400px', title: 'Add Contact Info' }) ;
+    $('#contactOpenBtn').click( 
+	function() { 
+	    $('#flash_notice').fadeOut('slow');
+	    $('#newContactPopup').dialog('open');
+	    
+	}
+    );
+    $('#newGroupPopup').dialog({ autoOpen : false, width: '400px', title: 'Add Group Info' }) ;
+    $('#groupOpenBtn').click( 
+	function() { 
+	    $('#flash_notice').fadeOut('slow');
+	    $('#newGroupPopup').dialog('open');
+	    
+	}
+    );
+    $('#addContactPopup').dialog({ autoOpen : false, width: '400px', title: 'Subscribe to this Group' }) ;
+    $('a[id^="addContactBtn"]').click( function() { 
 	
+	var notice = $('#flash_notice') ; 
+	notice.fadeOut('slow');
+	var group_id = $(this).attr('id').replace(/addContactBtn/g, "");
+
+	$('#addContactForm input[id=group_id]').val(group_id);
+	$('#addContactPopup').dialog('open');
     });
+    
+});
